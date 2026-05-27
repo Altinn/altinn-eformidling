0c5ec7e41d4b1bd70d44cf51b7c56f78a793f06d
Bakken <thomas.bakken@digdir.no>
2026-05-27 08:46:03 +0200

    Update url to statuscheck

--- tests/k6/src/config.js
+++ tests/k6/src/config.js
@@ -12,7 +12,7 @@ export let baseUrl = baseUrls[environment];
 // Integration point
 export var integrationPoint = {
   status:
-  "https://platform." + baseUrl + "/eformidling/api/statuses/",
+  "https://platform." + baseUrl + "/eformidling/api/statuses",
   availability:
   "https://platform." + baseUrl + "/eformidling/api/manage/availability/",
   conversations:
